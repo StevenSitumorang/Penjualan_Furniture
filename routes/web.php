42b3db25367b1ec2bfa686b72189e8401a2c72fb
salamkenalfajar <159238572+salamkenalfajar@users.noreply.github.com>
2024-05-28 22:51:38 +0700

    Halaman Crud

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/create', [ProductController::class, 'create'])->name('create');
+Route::post('/create', [ProductController::class, 'save']);
+Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('delete');
+Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('edit');
+Route::post('/edit/{id}', [ProductController::class, 'update']);
 
 //Produk Routes 
 Route::get('/Produk_furniture', [ProdukController::class, 'Produk']);
