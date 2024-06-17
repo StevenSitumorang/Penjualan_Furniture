3cccf0652aa95d0c58bd6c5f2c32d58dc8273d05
Kelompok 3 <Stevensitumorang08@gmail.com>
2024-06-17 22:41:56 +0700

    merubah routes home

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/penjual/product', [ProductController::class, 'index'])->name('home');
 Route::get('/create', [ProductController::class, 'create'])->name('create');
 Route::post('/create', [ProductController::class, 'save']);
 Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('delete');
