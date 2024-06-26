2ff25d0d6550b99528c18bc6794631d560da375d
salamkenalfajar <159238572+salamkenalfajar@users.noreply.github.com>
2024-06-26 12:53:40 +0700

    Untuk directory Main

---
+++ Crud/crud/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Resource route for products
+Route::resource('products', ProductController::class);
+
+// Optional: Additional routes if you need specific custom routes
+// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
