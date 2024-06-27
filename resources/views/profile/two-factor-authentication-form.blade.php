e59f61d48e4f5a1e050ec775b86dfd05e6fe3390
Kelompok 3 <Stevensitumorang08@gmail.com>
2024-06-27 19:01:30 +0700

    Perbaikian Profile

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -9,7 +9,7 @@
 
     <x-slot name="content">
         <h3 class="text-lg font-medium text-gray-900">
-            @if ($enabled)
+            @if ($this->enabled)
                 @if ($showingConfirmation)
                     {{ __('Finish enabling two factor authentication.') }}
                 @else
@@ -26,7 +26,7 @@
             </p>
         </div>
 
-        @if ($enabled)
+        @if ($this->enabled)
             @if ($showingQrCode)
                 <div class="mt-4 max-w-xl text-sm text-gray-600">
                     <p class="font-semibold">
@@ -39,12 +39,12 @@
                 </div>
 
                 <div class="mt-4 p-2 inline-block bg-white">
-                    {!! $user->twoFactorQrCodeSvg() !!}
+                    {!! $this->user->twoFactorQrCodeSvg() !!}
                 </div>
 
                 <div class="mt-4 max-w-xl text-sm text-gray-600">
                     <p class="font-semibold">
-                        {{ __('Setup Key') }}: {{ decrypt($user->two_factor_secret) }}
+                        {{ __('Setup Key') }}: {{ decrypt($this->user->two_factor_secret) }}
                     </p>
                 </div>
 
@@ -52,8 +52,9 @@
                     <div class="mt-4">
                         <x-label for="code" value="{{ __('Code') }}" />
 
-                        <x-input id="code" type="text" name="code" class="block mt-1 w-1/2" inputmode="numeric" autofocus
-                            autocomplete="one-time-code" wire:model="code" wire:keydown.enter="confirmTwoFactorAuthentication" />
+                        <x-input id="code" type="text" name="code" class="block mt-1 w-1/2" inputmode="numeric" autofocus autocomplete="one-time-code"
+                            wire:model="code"
+                            wire:keydown.enter="confirmTwoFactorAuthentication" />
 
                         <x-input-error for="code" class="mt-2" />
                     </div>
@@ -68,7 +69,7 @@
                 </div>
 
                 <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 rounded-lg">
-                    @foreach (json_decode(decrypt($user->two_factor_recovery_codes), true) as $code)
+                    @foreach (json_decode(decrypt($this->user->two_factor_recovery_codes), true) as $code)
                         <div>{{ $code }}</div>
                     @endforeach
                 </div>
@@ -76,7 +77,7 @@
         @endif
 
         <div class="mt-5">
-            @if (!$enabled)
+            @if (! $this->enabled)
                 <x-confirms-password wire:then="enableTwoFactorAuthentication">
                     <x-button type="button" wire:loading.attr="disabled">
                         {{ __('Enable') }}
